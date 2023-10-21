13e7d4b2aec10858915b30995f590a147808096d
CoelhoESC <everton_esc1@hotmail.com>
2023-10-21 16:09:01 -0300

    Foi adicionado ao projeto Jquery data base, agora na pagina de registro temos a opçao de buscar e paginação

--- Projeto_pimWEB/wwwroot/js/site.js
+++ Projeto_pimWEB/wwwroot/js/site.js
@@ -4,3 +4,35 @@
 // Write your JavaScript code.
 
 /* Handlers */
+
+$(document).ready(function () {
+    $('#table-registro').DataTable({
+        "ordering": true,
+        "paging": true,
+        "searching": true,
+        "oLanguage": {
+            "sEmptyTable": "Nenhum registro encontrado na tabela",
+            "sInfo": "Mostrar _START_ até _END_ de _TOTAL_ registro",
+            "sInfoEmpty": "Mostrar 0 até 0 de 0 registros",
+            "sInfoFiltered": "(Filtrar de _MAX_ total regiistros)",
+            "sInfoPostFix": "",
+            "sInfoThousands": ".",
+            "sLengthMenu": "Mostrar _MENU_ registro por pagina",
+            "sLoadingRecords": "Carregando...",
+            "sProcessing": "Processando...",
+            "sZeroRecords": "Nenhum registro encontrado",
+            "sSearch": "Procurar por ",
+            "oPaginate": {
+                "sNext": 'Proximo',
+                "sPrevious": "Anterior",
+                "sFirst": "Primeiro",
+                "sLast": "Ultimo"
+            },
+            "oAria": {
+                "sSortAscending": "Ordenar colunas de forma ascendente",
+                "sSortDescending": "Ordenar colunas de forma descendente"
+            }
+        }
+    });
+
+});
